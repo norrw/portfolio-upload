fbee4fa6cf76e3870c914772bbd6c1040f80b5ac
norrw <willnorrislenovo@outlook.com>
2021-03-16 21:25:31 +0000

    Added my information to website

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,11 @@ const Home = () => {
         <h1 className='text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name'>
           Hi. My name is Will!
         </h1>
+        <h3>I'm an motivated economics graduate turned web developer!</h3>
+        <h3>
+          <strong>My technical skills:</strong> HTML, CSS, JavaScript, React,
+          Redux, Express, Node.js, MongoDB, PostgreSQL, Git.
+        </h3>
       </section>
     </main>
   );
