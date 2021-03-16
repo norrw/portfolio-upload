186399455294369569da9dc86d06835c7f534df6
norrw <willnorrislenovo@outlook.com>
2021-03-16 12:41:29 +0000

    removed post feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,13 +15,13 @@ const Navbar = () => {
           >
             Will Norris
           </NavLink>
-          <NavLink
+          {/* <NavLink
             to='/post'
             className='inline-flex items-center py-3 px-3 my-6 rounded-text-red-200 hover:text-green-800'
             activeClassName='text-red-100 bg-red-700'
           >
             Posts
-          </NavLink>
+          </NavLink> */}
           <NavLink
             to='/project'
             className='inline-flex items-center py-3 px-3 my-6 rounded-text-red-200 hover:text-green-800'
